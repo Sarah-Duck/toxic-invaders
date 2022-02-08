91891c4e723bbdae22d0f2b61c8d51f90cb2beeb
Ribbon <ambiguousmachine@gmail.com>
2022-02-07 16:38:45 -0800

    updated bg a bit and rearranged first sprite page

--- background.lua
+++ background.lua
@@ -39,30 +39,52 @@ function drawbg()
         pset(((-t()+i*2)*(i/32))%128, sin(i*0.536382)*64+64, 4)
     end
 
-    --static planet
-    spr(201,110-t()/4,62,7,3)
-    spr(249,102-t()/4,86,7,1)
+    --ringed planet
+    spr(201,130-t(),76,7,3)
+    spr(249,122-t(),100,7,1)
     
     --clouds
     scrollingcircles(-9, 24, 22, 22, 4)
     scrollingcircles(-14, 40, 25, 20, 13)
-    scrollingsprite(194, 60, 16, 2, 1, 30)
-    scrollingsprite(196, 100, 18, 2, 1, 34)
-    scrollingsprite(198, 20, 20, 2, 1, 38)
+
+    --sprites
+    scrollingsprite(194, 20, 16, 2, 1, 34)
+    scrollingsprite(196, 40, 18, 2, 1, 34)
+    scrollingsprite(198, 20, 20, 2, 1, 34)
 
     camera((sin(shake+t())*shake)/2, (sin(shake+t()/2.1)*shake)/2)
     --landscape
-    scrollingcircles(110, 72, 25, 20, 8)
+    scrollingcircles(125, 72, 25, 20, 8)
 
-    --buildings
-    scrollingsprite(231,100,65+16,2,2,78)
-    scrollingsprite(226,65,67+16,2,2,80)
-    scrollingsprite(192,55,69,2,4,81)
-    scrollingsprite(230,85,70+16,1,2,82)
-    scrollingsprite(228,40,72+16,2,2,84)
-    scrollingsprite(192,20,75,2,4,86)
-    scrollingsprite(192,120,80,2,4,88)
+
+    --scrolling sprites
+    --pine trees
+    scrollingsprite(231,20,77+16,2,2,72)
+    scrollingsprite(231,70,78+16,2,2,72)
+    scrollingsprite(231,80,78+16,2,2,72)
+    scrollingsprite(231,100,79+16,2,2,72)
+    scrollingsprite(231,120,80+16,2,2,72)
+    --small buildings
+    scrollingsprite(226,15,80+16,2,2,78)
+    scrollingsprite(226,65,80+16,2,2,78)
+    scrollingsprite(226,95,80+16,2,2,78)
+    scrollingsprite(226,115,80+16,2,2,78)
+    --totem poles
+    scrollingsprite(230,25,83+16,1,2,82)
+    scrollingsprite(230,65,84+16,1,2,82)
+    scrollingsprite(230,85,85+16,1,2,82)
+    --dead trees
+    scrollingsprite(228,30,85+16,2,2,84)
+    scrollingsprite(228,75,86+16,2,2,84)
+    scrollingsprite(228,100,87+16,2,2,84)
+    scrollingsprite(228,120,88+16,2,2,84)
+    --large buildings (front)
+    scrollingsprite(192,55,94,2,4,90)
+    scrollingsprite(192,20,91,2,4,90)
+    scrollingsprite(192,100,90,2,4,90)
+    scrollingsprite(192,130,92,2,4,90)
+
 
     camera(sin(shake+t())*shake, sin(shake+t()/2.1)*shake)
-    scrollingcircles(135, 100, 30, 32, 15)
+    scrollingcircles(154, 100, 30, 32, 2)
 end
