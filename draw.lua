14e414cc4cd593868eebbfeda004edb263e251e4
1029chris <1029chris@gmail.com>
2022-02-08 19:43:46 -0800

    logo function

--- draw.lua
+++ draw.lua
@@ -13,4 +13,43 @@ pal(0,130,1)
 
 drawbg()
 drawobjs()
-print(#obj)
+--print(#obj)
+function drawlogo(x,y)
+    --default values
+    x = x or 24
+    y = y or 18
+    --this is a horrible way to draw the logo, but I'm doing it anyways.
+    -- muahahahahahahaha!!!!!!
+    sspr(0,24,16,8,x-1,y+2,32,16) --to
+    sspr(16,24,8,8,x+53,y+2,16,16) --ic
+    sspr(0,24,16,8,x-1,y+1,32,16) --to
+    sspr(16,24,8,8,x+53,y+1,16,16) --ic
+    --X
+    sspr(24,24,8,8,x+26,y-6,16,16)
+    sspr(24,24,8,8,x+42,y-6,16,16,true)
+    sspr(32,24,8,8,x+26,y+10,16,16, true)
+    sspr(32,24,8,8,x+42,y+10,16,16)
+    --X
+    sspr(24,24,8,8,x+26,y-8,16,16)
+    sspr(24,24,8,8,x+42,y-8,16,16,true)
+    sspr(32,24,8,8,x+26,y+8,16,16, true)
+    sspr(32,24,8,8,x+42,y+8,16,16)
+    --font
+    local text = "i n v a d e r s"
+    print(text, x+10, y+21, 3)
+    print(text, x+10, y+19, 3)
+    print(text, x+11, y+20, 3)
+    print(text, x+9, y+20, 3)
+    print(text, x+9, y+21, 3)
+    print(text, x+11, y+21, 3)
+    print(text, x+9, y+19, 3)
+    print(text, x+11, y+19, 3)
+    print(text, x+11, y+21, 3)
+    print(text, x+9, y+22, 3)
+    print(text, x+11, y+22, 3)
+    print(text, x+10, y+22, 3)
+    print(text, x+10, y+20, 14)
+    clip(x+10, y+20, 60, 3)
+    print(text, x+10, y+20, 11)
+    clip()
+end
